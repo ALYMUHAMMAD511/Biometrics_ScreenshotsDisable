3e13704c12d7da53cd37be1abd820c91c4fa64d9
alymu <alym7mdd@gmail.com>
2025-02-05 16:24:46 +0200

    ADD NAV GRAPH

---
+++ app/src/main/java/com/example/biometrics_screenshotsdisable/Fragment1.kt
@@ -0,0 +1,55 @@
+package com.example.biometrics_screenshotsdisable
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+
+class Fragment1 : Fragment() {
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Toast.makeText(context, "onAttach", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        Toast.makeText(context, "onCreateView", Toast.LENGTH_SHORT).show()
+        return inflater.inflate(R.layout.fragment_1, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(context, "onStart", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(context, "onResume", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(context, "onPause", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Toast.makeText(context, "onDestroyView", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Toast.makeText(context, "onDetach", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(context, "onDestroy", Toast.LENGTH_SHORT).show()
+    }
+}
